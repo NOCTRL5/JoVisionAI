41eda2249104cd3d8791b058e2b306987cf41385
NOCTRL5 <osamatawara7@gmail.com>
2025-07-07 15:52:36 +0300

    imported os

--- Task 3/main.py
+++ Task 3/main.py
@@ -1,8 +1,11 @@
 import sys
 from PIL import Image
 import pytesseract
+import os
+
 
 date =[]
 img_directory = f"{os.curdir}/Task 3/task3images/"
 image_files = [f for f in os.listdir(img_directory) if f.endswith('.jpg')]
 image_files.sort(key=lambda x: int(os.path.splitext(x.replace('.jpg', ''))[0]))
+
